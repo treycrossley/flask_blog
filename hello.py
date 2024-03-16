95e607c7241a427c474d1ab8a0cf355194b0e487
Trey Crossley <treycrossley@gmail.com>
2024-03-15 23:26:28 -0700

    sqlite and alchemy initial implementation

--- hello.py
+++ hello.py
@@ -2,16 +2,35 @@
 from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 # Create Flask instance
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config["SECRET_KEY"] = "1234"
+
+db = SQLAlchemy(app)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 
 # Form Class
 class NamerForm(FlaskForm):
     """class for asking user their name"""
     name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired(), Email()])
     submit = SubmitField("Submit")
 
 
@@ -37,6 +56,23 @@ def name():
 def user(name):
     """directs to user page"""
     return render_template("user.html", name=name)
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User added!!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template('add_user.html', form=form, name=name, our_users=our_users)
 
 # Invalid URL page
 @app.errorhandler(404)
