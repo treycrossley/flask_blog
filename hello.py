1ae441ff9e32f7cb70ca59f2386a126439b1ffe9
Trey Crossley <treycrossley@gmail.com>
2024-03-15 20:48:00 -0700

    babys first form

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 """For flask library and template rendering for our web app"""
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create Flask instance
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "1234"
+
+# Form Class
+class NamerForm(FlaskForm):
+    """class for asking user their name"""
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 # Create a route decorator
@@ -12,6 +22,15 @@ def index():
     first_name = "Trey"
     return render_template("index.html", first_name=first_name)
 
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    """directs to user form"""
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name = name, form = form)
 
 @app.route('/user/<name>')
 def user(name):
@@ -29,3 +48,4 @@ def invalid_url(e):
 def server_err(e):
     """handles error case for when server errors occur"""
     return render_template("500.html"), 500
+
