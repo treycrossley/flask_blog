78b38914f19a6dcdc3944bc953f3c66e65e6db4d
Trey Crossley <treycrossley@gmail.com>
2024-03-15 22:12:39 -0700

    flash messages

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 """For flask library and template rendering for our web app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -30,6 +30,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form succesfully submitted!!")
     return render_template("name.html", name = name, form = form)
 
 @app.route('/user/<name>')
