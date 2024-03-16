4b60ee1bc8d3f2f552da9dd8edbae9d3ea5cc5b0
Trey Crossley <treycrossley@gmail.com>
2024-03-15 18:46:03 -0700

    render template initial implementation

--- hello.py
+++ hello.py
@@ -8,4 +8,9 @@ app = Flask(__name__)
 # Create a route decorator
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
+
+
+@app.route('/user/<name>')
+def user(name):
+    return "<h1>HELLO {}</h1>".format(name)
