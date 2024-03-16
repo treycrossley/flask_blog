1c408228daa84c67a1e68c7ac817307d3d5104a5
Trey Crossley <treycrossley@gmail.com>
2024-03-15 19:46:09 -0700

    basic error pages

--- hello.py
+++ hello.py
@@ -15,3 +15,13 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", name=name)
+
+# Invalid URL page
+@app.errorhandler(404)
+def invalid_url(e):
+    return render_template("404.html"), 404
+
+# Server error
+@app.errorhandler(500)
+def invalid_url(e):
+    return render_template("500.html"), 500
