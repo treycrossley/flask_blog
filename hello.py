a296bd9bf2a12741a6d3fdc559d49decf5a3b8fa
Trey Crossley <treycrossley@gmail.com>
2024-03-16 18:10:59 -0700

    individual blog post page

--- hello.py
+++ hello.py
@@ -37,6 +37,11 @@ def posts():
     posts = Posts.query.order_by(Posts.date_posted)
     return render_template("posts.html", posts=posts)
 
+@app.route('/posts/<int:id>')
+def post(id):
+    post = Posts.query.get_or_404(id)
+    return render_template("post.html", post=post)
+
 @app.route('/add-post', methods=['GET', 'POST'])
 def add_post():
     form = PostForm()
