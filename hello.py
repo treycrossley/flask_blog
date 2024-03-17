b20bb03bb4403d3a60e31f57baecd5279c8d51c1
Trey Crossley <treycrossley@gmail.com>
2024-03-16 18:39:45 -0700

    edit post

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 """For flask library and template rendering for our web app"""
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo, Length
@@ -40,7 +40,29 @@ def posts():
 @app.route('/posts/<int:id>')
 def post(id):
     post = Posts.query.get_or_404(id)
-    return render_template("post.html", post=post)
+    return render_template('post.html', post=post)
+  
+
+@app.route('/posts/edit/<int:id>', methods=['GET','POST'])
+def edit_post(id):
+    post = Posts.query.get_or_404(id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.author = form.author.data
+        post.slug = form.slug.data
+        post.content = form.content.data
+
+        db.session.add(post)
+        db.session.commit()
+        flash('Post has been updated')
+
+        return redirect(url_for('post', id=post.id))
+    form.title.data=post.title
+    form.author.data=post.author
+    form.slug.data=post.slug
+    form.content.data=post.content
+    return render_template('edit_post.html',form=form)
 
 @app.route('/add-post', methods=['GET', 'POST'])
 def add_post():
