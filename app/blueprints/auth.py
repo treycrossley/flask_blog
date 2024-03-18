c8072aa4bf585823b903af758e7bcc9012e20e34
Trey Crossley <treycrossley@gmail.com>
2024-03-17 22:42:14 -0700

    virtual env needed a nuclear reset

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, redirect, request, flash, url_for
+from flask import Blueprint, render_template, redirect, flash, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 from app.models import Users
 from app.forms import LoginForm
-from app.extensions import db, bcrypt
-from flask_login import current_user, login_required, login_user, logout_user
+from app.extensions import bcrypt
 
+
+# Create a Blueprint for authentication-related routes
 auth_bp = Blueprint(
     "auth", __name__, url_prefix="/auth", template_folder="../../templates"
 )
@@ -11,29 +13,45 @@ auth_bp = Blueprint(
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
+    """Route for user login.
+
+    Renders the login form for GET requests and processes
+    login attempts for POST requests.
+
+    """
     form = LoginForm()
+
     if form.validate_on_submit():
-        try:
-            user = Users.query.filter_by(username=form.username.data).first()
-            if user:
-                if bcrypt.check_password_hash(user.password_hash, form.password.data):
-                    login_user(user)
-                    flash("Login sucessful!")
-                    if current_user.is_admin:
-                        return redirect(url_for("general.admin"))
-                    return redirect(url_for("general.home"))
+        # Attempt to retrieve the user from the database
+        user = Users.query.filter_by(username=form.username.data).first()
+
+        if user:
+            # Check if the provided password matches the stored hash
+            if bcrypt.check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                flash("Login successful!")
+
+                # Redirect to the appropriate page based on user role
+                if current_user.is_admin:
+                    return redirect(url_for("general.admin"))
                 else:
-                    flash("Wrong Password - Try again!")
+                    return redirect(url_for("general.home"))
             else:
-                flash("User does not exist")
-        except Exception:
-            flash("Could not get User from db")
+                flash("Wrong Password - Try again!")
+        else:
+            flash("User does not exist")
+
     return render_template("login.html", form=form)
 
 
 @auth_bp.route("/logout", methods=["GET", "POST"])
 @login_required
 def logout():
+    """Route for user logout.
+
+    Logs out the current user and redirects to the login page.
+
+    """
     logout_user()
     flash("You have been logged out!")
     return redirect(url_for("auth.login"))
